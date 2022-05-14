bca98a5ef0526e953c4fb17af74104ff35ec2bc0
bkfan1 <blossomsc15@gmail.com>
2022-05-13 22:33:47 -0400

    Testing grid styles

--- src/components/PricingSection.js
+++ src/components/PricingSection.js
@@ -15,12 +15,11 @@ export default function PricingSection({ children, emoji, title }) {
         display="flex"
         flexDirection="column"
         alignItems="center"
-        width="100%"
         marginBottom="20px"
       >
         <Heading color="white">{`${emoji} ${title}`}</Heading>
 
-        <Grid as="div" templateColumns='repeat(2, 1fr)' gap="15px" className="priceCardsGrid">{children}</Grid>
+        <Grid gridTemplateColumns="repeat(1, 1fr))" gap="15px" className="priceCardsGrid">{children}</Grid>
       </Container>
     </>
   );
